7961494db60756ed3647c1dc38ccdb28d24fc793
chinwemartins02 <chinwe_martins@yahoo.com>
2025-09-11 14:02:18 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Mobile Menu Toggle
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
+  });
